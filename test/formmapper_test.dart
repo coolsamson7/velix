15b346a374902ac7421ce2b76f9ebb6f038f9336
Andreas Ernst <andreas.ernst7@gmail.com>
2025-08-19 13:23:42 +0200

    new tests

--- test/formmapper_test.dart
+++ test/formmapper_test.dart
@@ -6,10 +6,11 @@ import "main.dart";
 import 'main.type_registry.g.dart';
 
 void main() {
+  // initialize
+
   registerAllDescriptors();
   registerWidgets();
 
-  var price =  Money(currency: "EU", value: 1);
   var product = Product(name: 'product', price: Money(currency: "EU", value: 1), status: Status.available);
 
   testWidgets('deferred & mutable instance', (WidgetTester tester) async {
@@ -206,7 +207,70 @@ void main() {
     expect(moneyResult.value, equals(2));
   });
 
+
+  testWidgets('two-way & immutable instance', (WidgetTester tester) async {
+    // create mapper
+
+    var price =  Money(currency: "EU", value: 1);
+
+    var mapper = FormMapper(instance: price, twoWay: true);
+
+    bool dirty = false;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Builder(
+            builder: (context) {
+              return Form(
+                key: mapper.getKey(),
+                child: Column(
+                  children: [
+                    mapper.bind<TextFormField>(
+                      context: context,
+                      path: 'value',
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+
+    final valueFinder = find.byKey(const Key('value'));
+
+    // one-way mapper
+
+    mapper.isDirty.addListener(() {
+      dirty = mapper.isDirty.value;
+    });
+
+    // set value
+
+    mapper.setValue(price);
+
+    expect(dirty, equals(false));
+
+    // change currency
+
+    await tester.enterText(valueFinder, '2');
+
+    expect(dirty, equals(true));
+
+    // commit
+
+    mapper.commit();
+
+    expect(dirty, equals(false));
+
+    // check text
+
+    expect(mapper.instance.currency, equals('EU'));
+    expect(mapper.instance.value, equals(2));
+  });
+
   // TODO testWidgets('two-way & mutable instance', (WidgetTester tester) async {});
-  // TODO testWidgets('two-way & immutable instance', (WidgetTester tester) async {});
   // TODO testWidgets('two-way & immutable instance, not all fields mapped', (WidgetTester tester) async {});
 }

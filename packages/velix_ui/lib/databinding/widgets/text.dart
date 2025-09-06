9314bde146d5a62c8ea48b51a4abfa3ffcccf944
Andreas Ernst <andreas.ernst7@gmail.com>
2025-09-06 11:19:13 +0200

    chore: removed obsolete import

--- packages/velix_ui/lib/databinding/widgets/text.dart
+++ packages/velix_ui/lib/databinding/widgets/text.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:velix/velix.dart';
-import 'package:velix_i18n/velix_i18n.dart';
 
 import '../form_mapper.dart';
 import '../valued_widget.dart';

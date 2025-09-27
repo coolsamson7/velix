6c5da878b6a7fb2aa258f39bd482d3d424bc7926
Andreas Ernst <andreas.ernst7@gmail.com>
2025-09-27 10:54:55 +0200

    added overlay

--- packages/velix_editor/lib/property_panel/editor/code_editor.dart
+++ packages/velix_editor/lib/property_panel/editor/code_editor.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart' hide Autocomplete;
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:velix/reflectable/reflectable.dart';
-import 'package:velix_di/di/di.dart';
 
 import '../../actions/autocomplete.dart';
 import '../../actions/types.dart';
@@ -10,13 +9,7 @@ import '../../commands/command_stack.dart';
 import '../../util/message_bus.dart';
 import '../editor_builder.dart';
 
-// we currently need a dummy class so that it doesn't conflict with the real string editor :-(
-class Code {}
-
-@Injectable()
 class CodeEditorBuilder extends PropertyEditorBuilder<Code> {
-  // override
-
   @override
   Widget buildEditor({
     required MessageBus messageBus,
@@ -34,17 +27,13 @@ class CodeEditorBuilder extends PropertyEditorBuilder<Code> {
   }
 }
 
-class CodeEditor extends StatefulWidget {
-  // instance data
+class Code {}
 
+class CodeEditor extends StatefulWidget {
   final dynamic value;
   final ValueChanged<dynamic> onChanged;
 
-  // constructor
-
   const CodeEditor({super.key, required this.value, required this.onChanged});
-
-  // override
 
   @override
   State<CodeEditor> createState() => _CodeEditorState();
@@ -58,8 +47,6 @@ class CompletionItem {
 }
 
 class _CodeEditorState extends State<CodeEditor> {
-  // instance data
-
   late TextEditingController _controller;
   late FocusNode _focusNode;
   List<CompletionItem> _matches = [];
@@ -69,7 +56,8 @@ class _CodeEditorState extends State<CodeEditor> {
   int _originalCursorPos = 0;
   late Autocomplete autocomplete;
 
-  // internal
+  final LayerLink _layerLink = LayerLink();
+  OverlayEntry? _overlayEntry;
 
   Iterable<CompletionItem> suggestions(String pattern, int offset) {
     try {
@@ -77,10 +65,10 @@ class _CodeEditorState extends State<CodeEditor> {
           .suggest(pattern, cursorOffset: offset)
           .map((suggestion) => CompletionItem(
           label: suggestion.suggestion,
-          icon: suggestion.type == "field" ? Icons.data_object : Icons.functions
-      ));
-    }
-    catch (e) {
+          icon: suggestion.type == "field"
+              ? Icons.data_object
+              : Icons.functions));
+    } catch (e) {
       return [];
     }
   }
@@ -89,15 +77,15 @@ class _CodeEditorState extends State<CodeEditor> {
     if (_isUpdatingCompletion) return;
 
     final cursorPos = _controller.selection.baseOffset;
-    if (cursorPos < 0) return;
+    if (cursorPos < 0) {
+      _removeOverlay();
+      return;
+    }
 
-    // Store the original state when user types
     _originalText = _controller.text;
     _originalCursorPos = cursorPos;
 
     final matches = suggestions(_controller.text, cursorPos).toList();
-
-    // callback
 
     widget.onChanged(_originalText);
 
@@ -105,6 +93,12 @@ class _CodeEditorState extends State<CodeEditor> {
       _matches = matches;
       _selectedIndex = matches.isNotEmpty ? 0 : -1;
     });
+
+    if (_matches.isNotEmpty && _focusNode.hasFocus) {
+      _showOverlay();
+    } else {
+      _removeOverlay();
+    }
 
     _updateInlineCompletion();
   }
@@ -116,7 +110,6 @@ class _CodeEditorState extends State<CodeEditor> {
 
     final completion = _matches[_selectedIndex].label;
 
-    // Find word start
     int wordStart = _originalCursorPos;
     while (wordStart > 0 && _isWordChar(_originalText[wordStart - 1])) {
       wordStart--;
@@ -150,13 +143,14 @@ class _CodeEditorState extends State<CodeEditor> {
   void _acceptCompletion() {
     if (_matches.isEmpty || _selectedIndex < 0) return;
 
-    // Move cursor to end of completion
     final selection = _controller.selection;
     if (selection.extentOffset > selection.baseOffset) {
       _isUpdatingCompletion = true;
       _controller.selection = TextSelection.collapsed(offset: selection.extentOffset);
       _isUpdatingCompletion = false;
     }
+
+    _removeOverlay();
 
     setState(() {
       _matches = [];
@@ -183,7 +177,6 @@ class _CodeEditorState extends State<CodeEditor> {
   void _dismissCompletion() {
     if (_matches.isEmpty) return;
 
-    // Restore original text and cursor position
     _isUpdatingCompletion = true;
     _controller.value = TextEditingValue(
       text: _originalText,
@@ -191,13 +184,94 @@ class _CodeEditorState extends State<CodeEditor> {
     );
     _isUpdatingCompletion = false;
 
+    _removeOverlay();
+
     setState(() {
       _matches = [];
       _selectedIndex = -1;
     });
   }
 
-  // override
+  void _showOverlay() {
+    _removeOverlay();
+
+    _overlayEntry = OverlayEntry(builder: (context) {
+      return Positioned(
+        width: 300,
+        child: CompositedTransformFollower(
+          link: _layerLink,
+          showWhenUnlinked: false,
+          offset: const Offset(0, 40),
+          child: Material(
+            elevation: 4,
+            borderRadius: BorderRadius.circular(6),
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxHeight: 150),
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                shrinkWrap: true,
+                itemCount: _matches.length,
+                itemBuilder: (context, index) {
+                  final item = _matches[index];
+                  final isSelected = index == _selectedIndex;
+                  return InkWell(
+                    onTap: () {
+                      setState(() {
+                        _selectedIndex = index;
+                      });
+                      _updateInlineCompletion();
+                      _acceptCompletion();
+                    },
+                    child: Container(
+                      padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                      color: isSelected ? Colors.blue.shade50 : null,
+                      child: Row(
+                        children: [
+                          Icon(
+                            item.icon ?? Icons.code,
+                            size: 16,
+                            color: isSelected ? Colors.blue.shade600 : Colors.grey.shade600,
+                          ),
+                          const SizedBox(width: 8),
+                          Expanded(
+                            child: Text(
+                              item.label,
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontFamily: 'monospace',
+                                fontWeight:
+                                isSelected ? FontWeight.w500 : FontWeight.normal,
+                                color:
+                                isSelected ? Colors.blue.shade800 : Colors.black87,
+                              ),
+                            ),
+                          ),
+                          if (isSelected)
+                            Icon(
+                              Icons.keyboard_return,
+                              size: 14,
+                              color: Colors.blue.shade400,
+                            ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+      );
+    });
+
+    Overlay.of(context).insert(_overlayEntry!);
+  }
+
+  void _removeOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+  }
 
   @override
   void didChangeDependencies() {
@@ -213,12 +287,18 @@ class _CodeEditorState extends State<CodeEditor> {
     _controller = TextEditingController(text: widget.value);
     _focusNode = FocusNode();
     _controller.addListener(_onTextChanged);
+    _focusNode.addListener(() {
+      if (!_focusNode.hasFocus) {
+        _removeOverlay();
+      }
+    });
   }
 
   @override
   void dispose() {
     _controller.dispose();
     _focusNode.dispose();
+    _removeOverlay();
 
     super.dispose();
   }
@@ -253,112 +333,20 @@ class _CodeEditorState extends State<CodeEditor> {
 
         return KeyEventResult.ignored;
       },
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // TextField with inline completion
-          TextField(
-            controller: _controller,
-            focusNode: _focusNode,
-            decoration: const InputDecoration(
-              hintText: "Type here for autocompletion...",
-              border: OutlineInputBorder(),
-              contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-            ),
-            style: const TextStyle(fontSize: 16, fontFamily: 'monospace'),
+      child: CompositedTransformTarget(
+        link: _layerLink,
+        child: TextField(
+          controller: _controller,
+          focusNode: _focusNode,
+          decoration: const InputDecoration(
+            hintText: "Type here for autocompletion...",
+            border: OutlineInputBorder(),
+            contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
           ),
-
-          // Compact suggestion list
-          if (_matches.isNotEmpty)
-            Container(
-              margin: const EdgeInsets.only(top: 4),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(6),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.08),
-                    blurRadius: 6,
-                    offset: const Offset(0, 2),
-                  ),
-                ],
-                border: Border.all(color: Colors.grey.shade300),
-              ),
-              constraints: const BoxConstraints(maxHeight: 150),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(6),
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: _matches.length,
-                  itemBuilder: (context, index) {
-                    final item = _matches[index];
-                    final isSelected = index == _selectedIndex;
-
-                    return Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        onTap: () {
-                          setState(() {
-                            _selectedIndex = index;
-                          });
-                          _updateInlineCompletion();
-                          _acceptCompletion();
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                          decoration: BoxDecoration(
-                            color: isSelected ? Colors.blue.shade50 : null,
-                          ),
-                          child: Row(
-                            children: [
-                              Icon(
-                                item.icon ?? Icons.code,
-                                size: 16,
-                                color: isSelected ? Colors.blue.shade600 : Colors.grey.shade600,
-                              ),
-                              const SizedBox(width: 8),
-                              Expanded(
-                                child: Text(
-                                  item.label,
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    fontFamily: 'monospace',
-                                    fontWeight: isSelected ? FontWeight.w500 : FontWeight.normal,
-                                    color: isSelected ? Colors.blue.shade800 : Colors.black87,
-                                  ),
-                                ),
-                              ),
-                              if (isSelected)
-                                Icon(
-                                  Icons.keyboard_return,
-                                  size: 14,
-                                  color: Colors.blue.shade400,
-                                ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-
-          // Compact hint
-          if (_matches.isNotEmpty)
-            Padding(
-              padding: const EdgeInsets.only(top: 4),
-              child: Text(
-                '↑↓ navigate • Tab/Enter/→ accept • Esc cancel',
-                style: TextStyle(
-                  fontSize: 11,
-                  color: Colors.grey.shade600,
-                  fontStyle: FontStyle.italic,
-                ),
-              ),
-            ),
-        ],
+          style: const TextStyle(fontSize: 16, fontFamily: 'monospace'),
+        ),
       ),
     );
   }
 }
+

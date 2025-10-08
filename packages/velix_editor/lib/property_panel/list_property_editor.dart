765689e885412f756ef5c914c226457e702cbce5
Andreas Ernst <andreas.ernst7@gmail.com>
2025-10-08 15:20:30 +0200

    + inside the rows

--- packages/velix_editor/lib/property_panel/list_property_editor.dart
+++ packages/velix_editor/lib/property_panel/list_property_editor.dart
@@ -41,38 +41,24 @@ class ListPropertyEditor extends PropertyEditorBuilder<List> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          children: [
-            const Spacer(),
-            IconButton(
-              icon: const Icon(Icons.add_circle_outline, size: 18, color: Colors.green),
-              tooltip: "Add item",
-              onPressed: addItem,
+        for (int i = 0; i < items.length; i++)
+          _HoverableListItem(
+            child: _buildElementEditor(
+              environment: environment,
+              messageBus: messageBus,
+              commandStack: commandStack,
+              property: property,
+              object: object,
+              elementType: elementType.type,
+              value: items[i],
+              onChanged: (newVal) {
+                items[i] = newVal;
+                onChanged(items);
+              },
             ),
-          ],
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            for (int i = 0; i < items.length; i++)
-              _HoverableListItem(
-                child: _buildElementEditor(
-                  environment: environment,
-                  messageBus: messageBus,
-                  commandStack: commandStack,
-                  property: property,
-                  object: object,
-                  elementType: elementType.type,
-                  value: items[i],
-                  onChanged: (newVal) {
-                    items[i] = newVal;
-                    onChanged(items);
-                  },
-                ),
-                onRemove: () => deleteItem(items[i]),
-              ),
-          ],
-        ),
+            onAdd: addItem,
+            onRemove: () => deleteItem(items[i]),
+          ),
       ],
     );
   }
@@ -109,10 +95,12 @@ class ListPropertyEditor extends PropertyEditorBuilder<List> {
 
 class _HoverableListItem extends StatefulWidget {
   final Widget child;
+  final VoidCallback onAdd;
   final VoidCallback onRemove;
 
   const _HoverableListItem({
     required this.child,
+    required this.onAdd,
     required this.onRemove,
   });
 
@@ -129,8 +117,8 @@ class _HoverableListItemState extends State<_HoverableListItem> {
       onEnter: (_) => setState(() => _hovering = true),
       onExit: (_) => setState(() => _hovering = false),
       child: Container(
-        // Removed vertical padding between items
         padding: const EdgeInsets.all(4),
+        margin: const EdgeInsets.symmetric(vertical: 2),
         decoration: BoxDecoration(
           border: Border.all(color: Colors.grey.shade300),
           borderRadius: BorderRadius.circular(4),
@@ -143,13 +131,32 @@ class _HoverableListItemState extends State<_HoverableListItem> {
                 Expanded(child: widget.child),
               ],
             ),
+
+            // Show icons on hover
             if (_hovering)
               Positioned(
-                top: 0,
-                right: 0,
-                child: GestureDetector(
-                  onTap: widget.onRemove,
-                  child: Icon(Icons.delete, size: 18, color: Colors.red.shade700),
+                top: 4,
+                right: 4,
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: widget.onAdd,
+                      child: Icon(
+                        Icons.add_circle_outline,
+                        size: 18,
+                        color: Colors.green.shade700,
+                      ),
+                    ),
+                    const SizedBox(width: 6),
+                    GestureDetector(
+                      onTap: widget.onRemove,
+                      child: Icon(
+                        Icons.remove_circle_outline,
+                        size: 18,
+                        color: Colors.red.shade700,
+                      ),
+                    ),
+                  ],
                 ),
               ),
           ],

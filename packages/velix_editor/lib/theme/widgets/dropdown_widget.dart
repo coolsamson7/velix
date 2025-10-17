924dbdd8152a0ccf81ef54d8c0afeeb3c57b5864
Andreas Ernst <andreas.ernst7@gmail.com>
2025-10-17 10:11:45 +0200

    added drop target

--- packages/velix_editor/lib/theme/widgets/dropdown_widget.dart
+++ packages/velix_editor/lib/theme/widgets/dropdown_widget.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart' hide WidgetBuilder;
 import 'package:velix_di/di/di.dart';
 
+import '../../commands/command_stack.dart';
+import '../../commands/reparent_command.dart';
 import '../../dynamic_widget.dart';
 import '../../edit_widget.dart';
 import '../../event/events.dart';
 import '../../metadata/type_registry.dart';
+import '../../metadata/widget_data.dart';
 import '../../metadata/widgets/dropdown.dart';
 import '../../metadata/widgets/for.dart';
 
@@ -100,27 +103,72 @@ class _DropDownWidgetState extends State<_DropDownWidget> {
 class DropDownEditWidgetBuilder extends WidgetBuilder<DropDownWidgetData> {
   final TypeRegistry typeRegistry;
 
-  DropDownEditWidgetBuilder({required this.typeRegistry}) : super(name: "dropdown", edit: true);
+  DropDownEditWidgetBuilder({required this.typeRegistry})
+      : super(name: "dropdown", edit: true);
 
   @override
-  Widget create(DropDownWidgetData data, Environment environment, BuildContext context) {
-    final items = <Widget>[];
+  Widget create(
+      DropDownWidgetData data, Environment environment, BuildContext context) {
+    return DragTarget<WidgetData>(
+      onWillAccept: (widget) => data.acceptsChild(widget!),
+      onAccept: (widget) {
+        environment.get<CommandStack>().execute(
+          ReparentCommand(
+            bus: environment.get<MessageBus>(),
+            widget: widget,
+            newParent: data,
+          ),
+        );
 
-    for (var childData in data.children) {
-        items.add(EditWidget(model: childData));
-      }
+        WidgetsBinding.instance.addPostFrameCallback((_) =>
+            environment.get<MessageBus>().publish(
+              "selection",
+              SelectionEvent(selection: widget, source: this),
+            ));
+      },
+      builder: (context, candidateData, rejectedData) {
+        final isActive = candidateData.isNotEmpty;
+        final hasChildren = data.children.isNotEmpty;
 
-    return IgnorePointer(
-      ignoring: true,
-      child: SizedBox(
-        width: double.infinity,
-        child: Column(
-          //value: null,
-          //hint: const Text('Select an item'), // TODO
-          children: items,
-          //onChanged: (_) {},
-        ),
-      ),
+        final fakeSelectedChild =
+        hasChildren ? EditWidget(model: data.children.first) : null;
+
+        return Container(
+          constraints: const BoxConstraints(minWidth: 120, minHeight: 40),
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: isActive ? Colors.blue : Colors.grey.shade400,
+              width: isActive ? 3 : 1,
+            ),
+            borderRadius: BorderRadius.circular(6),
+            color: isActive
+                ? Colors.blue.shade50
+                : Colors.grey.shade100.withOpacity(0.6),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: fakeSelectedChild ??
+                    Text(
+                      isActive ? 'Drop option here' : 'Select an item',
+                      style: TextStyle(
+                        color: isActive
+                            ? Colors.blue.shade700
+                            : Colors.grey.shade600,
+                        fontStyle: hasChildren
+                            ? FontStyle.normal
+                            : FontStyle.italic,
+                      ),
+                    ),
+              ),
+              const Icon(Icons.arrow_drop_down, color: Colors.grey),
+            ],
+          ),
+        );
+      },
     );
   }
 }
+
